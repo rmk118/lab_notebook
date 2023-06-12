45063f0927060366cd4a1c1d6a9880bc63493a71
rmk118 <rubykrasnow@gmail.com>
2023-06-12 15:52:10 -0400

    started data exploration

---
+++ exploring.R
@@ -0,0 +1,29 @@
+#Exploratory data analysis
+#June 12, 2023
+
+library(ggplot2)
+library(dplyr)
+
+# dat_all<- read.csv("22564_UNION_FSCS_SVLEN.csv")
+# head(dat_all)
+# summary(dat_all) #530652 observations of 13 vars
+# 
+# glimpse(dat_all)
+# 
+# name<-as.factor(dat_all$SCIENTIFIC_NAME)
+# summary(name)
+# 
+# data <- dat_all %>%
+#   filter(SCIENTIFIC_NAME == "Placopecten magellanicus (sea scallop)" | 
+#            SCIENTIFIC_NAME == "Placopecten magellanicus (sea scallop clapper)")
+
+# cruises <- read.csv("data/22564_SVDBS_CRUISES.csv")
+# svbio <- read.csv("data/22564_UNION_FSCS_SVBIO.csv", quote = "")
+# svsta <- read.csv("data/22564_UNION_FSCS_SVSTA.csv")
+
+svcat <- read.csv("data/22564_UNION_FSCS_SVCAT.csv")
+head(svcat)
+summary(svcat)
+scallops <- svcat %>% 
+  filter(SCIENTIFIC_NAME=="Placopecten magellanicus (sea scallop)" | 
+           SCIENTIFIC_NAME =="Placopecten magellanicus (sea scallop clapper)")
