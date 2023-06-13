1ca3a2af3eb88ce1fe0b41e9b995889a67a84803
rmk118 <rubykrasnow@gmail.com>
2023-06-13 10:22:21 -0400

    created Jun 13, merged cruise and scallop info

--- exploring.R
+++ exploring.R
@@ -1,5 +1,5 @@
 #Exploratory data analysis
-#June 12, 2023
+#June 13, 2023
 
 library(ggplot2)
 library(dplyr)
@@ -30,8 +30,35 @@ scallops <- svcat %>%
 rm(svcat)
 
 scallops <- scallops %>% 
-  mutate(id = as.factor(ID),
-         name = as.factor(SCIENTIFIC_NAME),
+  mutate(name = SCIENTIFIC_NAME,
          cruise6 = as.factor(CRUISE6),
          cruise = as.factor(CRUISE),
+         sex = as.factor(CATCHSEX),
+         stratum = as.factor(STRATUM),
          .keep = "unused")
+
+summary(scallops)
+
+scallops <- scallops %>% 
+  mutate(name = recode(name, "Placopecten magellanicus (sea scallop)" = "scallop", "Placopecten magellanicus (sea scallop clapper)" = "clapper"))
+
+summarise_all(scallops,n_distinct)
+
+#All have sex = 0, means unknown sex
+#SVSPP is species, listed as either 401 (scallop) or 400 (clammer), redundant info to species
+
+#Remove unnecessary variables
+scallops <- scallops %>% 
+  select(-c("sex", "STATUS_CODE", "SVSPP"))
+
+#ggplot(scallops, aes(x = cruise)) + geom_bar()
+ggplot(scallops, aes(x = cruise6)) + geom_bar()
+#ggplot(scallops, aes(x = stratum)) + geom_bar()
+
+cruises <- read.csv("data/22564_SVDBS_CRUISES.csv")
+cruises <- cruises %>% 
+  select(c("CRUISE6","SEASON","YEAR")) %>% 
+  mutate(cruise6 = as.factor(CRUISE6), .keep="unused")
+
+
+scallops <- left_join(scallops, cruises, by="cruise6")
