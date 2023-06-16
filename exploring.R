109232c417f458f46a1aa145b9b246e99947ebe2
rmk118 <rubykrasnow@gmail.com>
2023-06-16 14:34:25 -0400

    added updated survey data

--- exploring.R
+++ exploring.R
@@ -169,6 +169,7 @@ scallopsTest <- scallopsNew %>%
 
 library(sf)
 
+# SMAST Scallop data - the data I want, but averaged across years
 st_layers("data/SMAST_SCALLOP/SMAST_Scallops.gdb")
 test <- st_read("data/SMAST_SCALLOP/SMAST_Scallops.gdb")
 
@@ -180,7 +181,7 @@ test_geom <- st_geometry(test)
 par(mar = c(0,0,1,0))
 plot(test[6])
 
-
+# NEFSC scallop survey data - through 2015
 st_layers("~/Downloads/Fish/Fish.gdb")
 fish <- st_read("~/Downloads/Fish/Fish.gdb", layer="ScallopBiomass")
 
